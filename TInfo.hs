f7c65699959cfcae7f846d0cbd9f49dd84508bb3
DavidM <david.dmcbride@moxune.com>
2011-03-17 11:36:00 -0400

    Added filesizes to files and fixed formatting.

--- TInfo.hs
+++ TInfo.hs
@@ -48,24 +48,28 @@ printSummary filename bd = do
   let ann      = (fromBDataList . getMainAnnounce) bd
       annlist  = (fromBDataList . getAnnounceList) bd
       filelist = (fromBDataList . getFileList) bd :: [B.ByteString]
+      sizelist = (fromBDataList . getFileSize) bd :: [Integer]
       date     = (maybe (0 :: Integer) id . fromBData . headBDataList . getCreateDate) bd
       comment  = (maybe ("" :: B.ByteString) id . fromBData . headBDataList . getComment) bd
       numfiles = P.length filelist
-      filesize = (sum . fromBDataList . getFileSize) bd :: Integer
+      totalfilesize = (sum . fromBDataList . getFileSize) bd :: Integer
       sum      = L.foldl1' (+) --Why isn't there a strict sum function?
+      filesizelist = P.zip sizelist filelist
 
       fromBDataList :: forall a. (FromBData a) => [BData] -> [a]
       fromBDataList = catMaybes . P.map fromBData
       headBDataList = maybe (BString "") id . listToMaybe
 
+      printFile (size, filename) = printf "   %7s  %s\n" (formatSize size) filename
+
   printf "%s\n" (takeFileName filename)
-  printf "  Comment:     %s\n" comment
+  printf "  Comment:  %s\n" comment
   printf "  Date:     %s\n" =<< formatDateTime (fromIntegral date)
   printf "  Trackers: %s\n" (B.intercalate ", " (L.union ann annlist))
-  printf "  Size:     %s\n" (formatSize filesize)
+  printf "  Size:     %s\n" (formatSize totalfilesize)
   printf "  Files:    %d\n" numfiles
-  mapM_ (printf "    %s\n") filelist
-  B.putStrLn ""
+  mapM_ printFile filesizelist
+  printf "\n"
 
 getMainAnnounce = beAssocLookup "announce" >>> beString
 getAnnounceList = beAssocLookup "announce-list" >>> deep beString
@@ -83,11 +87,10 @@ getFileSize    = complex `orElse` simple
 
 formatSize :: Integer -> String
 formatSize i
-  | i < kilo  = printf "%dB" i
-  | i < meg   = printf "%.2fKB" (sizeFloat kilo)
-  | i < gig   = printf "%.2fMB" (sizeFloat meg)
-  | i < peta  = printf "%.2fGB" (sizeFloat gig)
-  | otherwise = printf "%dGB"   (sizeInt gig)
+  | i < meg   = printf "%.1fK" (sizeFloat kilo)
+  | i < gig   = printf "%.1fM" (sizeFloat meg)
+  | i < peta  = printf "%.1fG" (sizeFloat gig)
+  | otherwise = printf "%dG"   (sizeInt gig)
   where
     kilo = 1024
     meg  = kilo * 1024
